102843f99a88a8689b738adc880771070b7dab5e
Raj <rajanbabu.rajaraman@gmail.com>
2021-05-07 21:02:11 -0700

    Qtn1 updates

---
+++ EmployeeSQL/Table_Queries.sql
@@ -0,0 +1,10 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT
+	e.emp_no,
+	e.last_name,
+	e.first_name,
+	e.sex,
+	s.salary
+FROM employees e
+INNER JOIN salaries s ON
+e.emp_no = s.emp_no;
